ead7414c754daa904b4c779aa4f7865eaa60b5a5
NikitaS4 <nikita-s4astlivtsev2012@yandex.ru>
2020-12-03 12:11:15 +0300

    Split script for compilation
    Split script for compilation into 2 scripts: for python module
    building and for executable building

---
+++ Code/GBoosting/buildExe.sh
@@ -0,0 +1,6 @@
+# compile project to executable
+g++ -Wall -std=c++17 common/*.cpp common/*.h cppMain/*.cpp -o JITtrees
+
+# report
+echo build finished
+
--- Code/GBoosting/buildPy.sh
+++ Code/GBoosting/buildPy.sh
@@ -2,9 +2,6 @@
 
 c++ -O3 -Wall -shared -std=c++17 -fPIC `python3 -m pybind11 --includes` common/*.cpp pybind/*.cpp pybind/*.h common/*.h -o JITtrees`python3-config --extension-suffix`
 
-# compile project to executable
-g++ -Wall -std=c++17 common/*.cpp common/*.h cppMain/*.cpp -o JITtrees
-
 # report
 echo build finished
 
