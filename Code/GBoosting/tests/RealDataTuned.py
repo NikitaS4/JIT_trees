27761cddb3843adbb73a78f83a4b4b0edec9af51
NikitaS4 <nikita-s4astlivtsev2012@yandex.ru>
2021-04-14 16:23:42 +0300

    Update RealDataTuned script with thread cnt
    Add thread count to the RealDataTuned.py script

--- Code/GBoosting/tests/RealDataTuned.py
+++ Code/GBoosting/tests/RealDataTuned.py
@@ -20,7 +20,7 @@ import JITtrees
 def split_options(model_options):
     # splits model options to the ctor and fit options (for JITtrees model)
     ctor_keys = ['min_bins', 'max_bins', 'patience',
-                 'no_early_stopping']
+                 'no_early_stopping', 'thread_cnt']
     fit_keys = ['tree_count', 'tree_depth',
                 'feature_fold_size', 'learning_rate',
                 'early_stopping_delta', 'batch_part',
@@ -247,7 +247,8 @@ def tune_boston(folder, random_state=12):
         'batch_part': [1],
         'random_batches': [False],
         'random_hist_thresholds': [True],
-        'remove_regularization_later': [True]
+        'remove_regularization_later': [True],
+        'thread_cnt': [1]
     }
 
     tuning_params = {
@@ -294,7 +295,8 @@ def tune_diabetes(folder, random_state=12):
         'batch_part': [1],
         'random_batches': [True],
         'random_hist_thresholds': [True],
-        'remove_regularization_later': [True]
+        'remove_regularization_later': [True],
+        'thread_cnt': [1]
     }
 
     tuning_params = {
@@ -341,7 +343,8 @@ def tune_regression_100(folder, random_state=12):
         'batch_part': [1],
         'random_batches': [True],
         'random_hist_thresholds': [True],
-        'remove_regularization_later': [True]
+        'remove_regularization_later': [True],
+        'thread_cnt': [1]
     }
 
     tuning_params = {
@@ -390,7 +393,8 @@ def tune_regression_200(folder, random_state=12):
         'batch_part': [1],
         'random_batches': [True],
         'random_hist_thresholds': [True],
-        'remove_regularization_later': [True]
+        'remove_regularization_later': [True],
+        'thread_cnt': [1]
     }
     tuning_params = {
         'cb_grid': CatBoost_grid, 
@@ -452,7 +456,8 @@ def tune_supercond(folder, random_state=12):
         'batch_part': [0.6],
         'random_batches': [True],
         'random_hist_thresholds': [True],
-        'remove_regularization_later': [False, True]
+        'remove_regularization_later': [False, True],
+        'thread_cnt': [1]
     }
 
     tuning_params = {
